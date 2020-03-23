6535dbfb441f2bdadc9d50c927769b2cd36702f0
shanthi <abs.shanthiponnudurai@gmail.com>
2020-03-23 14:20:42 +0530

    'angular'

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb+srv://shanu:shanu@cluster0-hpjha.mongodb.net/demoDB?retryWrites=true&w=majority'
+};
---
+++ backend/server.js
@@ -0,0 +1,60 @@
+//import packages
+let express = require('express'),
+path = require('path'),
+mongoose = require('mongoose'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
+dbConfig = require('./database/db');
+
+//connecting with mongo db
+//declaring promise globally
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(()=>{
+    console.log("MongoDB Connected");
+    },
+    error =>{
+        console.log("MongoDB not Connected", + error)
+    }
+    );
+
+//setting up the port with express
+const employeeRoute = require('../backend/routes/employee.route');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
+app.use(cors());
+//to tell express where to check the static assets
+app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+
+app.use('/api', employeeRoute);
+
+//create port and assign number
+const port = process.env.PORT || 4000;
+
+//start the server
+const server = app.listen(port, ()=>{
+    console.log(`Server started on PORT: ${port}`);
+});
+
+// Find 404 and hand over to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+ });
+ 
+//error handler
+app.use(function (err, req, res, next) {
+    console.error(err.message); // Log error message in our server's console
+    if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+    res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+  });
+
+
+
+
+
